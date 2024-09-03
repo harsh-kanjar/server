b7f83492b90436cdcb0e48856d4d225ede97bb0e
harsh-kanjar <harshlankar07@gmail.com>
2024-09-03 22:26:38 +0530

    commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://harshkanjar07:bKqpBYVC9pR30yo8@cluster0.cg9ud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/"
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://harshkanjar07:bKqpBYVC9pR30yo8@cluster0.cg9ud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test"
 
 const connectToMongo = async () => {
     try {
